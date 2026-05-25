f7e36f1e8a075343510a619d8ee0897ef848d9ab
Pithak-00 <31059244+Pithak-00@users.noreply.github.com>
2026-05-25 16:26:31 +0900

    シェルスクリプト更新

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,6 +3,12 @@ set -e
 
 echo "BeforeInstall started"
 
+echo "=== Docker cleanup start ==="
+
+docker build prune -f
+
+echo "=== Docker cleanup done ==="
+
 mkdir -p /home/ec2-user/test
 
 echo "BeforeInstall completed"
