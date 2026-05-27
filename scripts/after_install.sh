5a0f581eb3a24d11b9a1ba11f0105a68e6c3be75
Pithak-00 <31059244+Pithak-00@users.noreply.github.com>
2026-05-27 17:38:15 +0900

    更新

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,12 +5,13 @@ echo "AfterInstall started"
 
 APP_DIR="/home/ec2-user/test/test_app"
 
-# 権限設定
-chown -R ec2-user:ec2-user $APP_DIR
-chmod -R 755 $APP_DIR
 
 # .env と DB をアプリディレクトリにコピー
 cp /home/ec2-user/persistent_data/.env $APP_DIR/.env
 cp /home/ec2-user/persistent_data/db.sqlite3 $APP_DIR/db.sqlite3
+
+# 権限設定
+chown -R ec2-user:ec2-user $APP_DIR
+chmod -R 755 $APP_DIR
 
 echo "AfterInstall completed"
