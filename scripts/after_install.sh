ee67ea6b82cd827c9fecf952648e0b4267136753
Pithak-00 <31059244+Pithak-00@users.noreply.github.com>
2026-05-27 09:25:21 +0900

    更新

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,10 +3,14 @@ set -e
 
 echo "AfterInstall started"
 
-chown -R root:root /home/ec2-user/test/test_app
-chmod -R 755 /home/ec2-user/test/test_app
+APP_DIR="/home/ec2-user/test/test_app"
 
-cp /home/ec2-user/persistent_data/.env  "$(pwd)/.env"
-cp /home/ec2-user/persistent_data/db.sqlite3 "$(pwd)/db.sqlite3"
+# 権限設定
+chown -R root:root $APP_DIR
+chmod -R 755 $APP_DIR
+
+# .env と DB をアプリディレクトリにコピー
+cp /home/ec2-user/persistent_data/.env $APP_DIR/.env
+cp /home/ec2-user/persistent_data/db.sqlite3 $APP_DIR/db.sqlite3
 
 echo "AfterInstall completed"
